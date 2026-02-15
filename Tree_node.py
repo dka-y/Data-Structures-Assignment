ab03fa496a65b798d82a9a83f0691eab9053da89
Jorbat Collins <jbcollins254@gmail.com>
2026-02-15 07:50:41 +0000

    completes the insert function

--- Tree_node.py
+++ Tree_node.py
@@ -2,4 +2,18 @@
 
 class BinaryTree:
     def __init__(self, data):
-        
+        self.data = data
+        self.left = None
+        self.right = None
+# I have added another class to handle the structure and behavior while the previous class handles the data
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if not self.root:
+            self.root = BinaryTree
+        else:
+            self._insert_recursive(self.root, data)
+
+
